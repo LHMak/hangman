75b4aca81bdd688b0aaf0993166c0cd236020e5c
LHMak <lhmakhdum@gmail.com>
2023-10-23 12:17:07 +0100

    Debugs play_game() function to end game when win/loss conditions satisfied

--- milestone_5.py
+++ milestone_5.py
@@ -39,17 +39,17 @@ class Hangman:
 
 
 def play_game(word_list):
-    num_lives = 5
+    num_lives = 3
     game = Hangman(word_list, num_lives)
     while True:
-        if num_lives == 0:
+        if game.num_lives == 0:
             print("You lost!")
             break
 
         elif game.num_letters > 0:
             game.ask_for_input()
 
-        elif num_lives > 0 and game.num_letters < 1:
+        elif game.num_lives > 0 and game.num_letters < 1:
             print("You won the game!")
             break
 
